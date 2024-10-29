211500eabc58eaa7861230f61be0f5ddb8d126a4
Ivan Shchekotov <64280999+isdevnull@users.noreply.github.com>
2024-10-29 20:12:54 +0300

    fix weight, grad norm computation

--- utils.py
+++ utils.py
@@ -1,21 +1,21 @@
 from collections import OrderedDict
-
+import math
 
 def compute_grad_norm(model):
-    norm = 0
+    norm = 0.0
     for p in model.parameters():
         cur_grad = p.grad.square().sum() if p.grad is not None else 0
         norm += cur_grad
     
-    return norm.cpu().sqrt()
+    return math.sqrt(norm)
 
 
 def compute_weight_norm(model):
-    norm = 0
+    norm = 0.0
     #norm_dict = OrderedDict()
     for n, p in model.named_parameters():
         cur_weight_norm = p.square().sum().cpu()
         #norm_dict[n] = cur_weight_norm.sqrt().cpu()
         norm += cur_weight_norm
     
-    return norm.cpu().sqrt()
+    return math.sqrt(norm)
